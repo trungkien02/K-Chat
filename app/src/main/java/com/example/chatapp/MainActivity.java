9851d765086861180fbe268b5459b489ae769def
trungkien02 <trungkien.mv123@gmail.com>
2022-04-29 21:09:08 +0700

    finsh-project

---
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
